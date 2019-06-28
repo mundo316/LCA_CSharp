1d339f21d879075925fd8eec0322e40293ebda6e
mundo316 <raymundo.garcia316@yahoo.com>
2019-06-27 19:09:22 -0500

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("sup");
+        }
+    }
+}
---
+++ project1/project1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace project1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            hello();
+
+            addition();
+
+            catDog();
+
+            oddEven();
+
+            inches();
+
+        }
+        public static void hello()
+        {
+            Console.WriteLine("Hello! What's your name?");
+
+            string name = Console.ReadLine();
+            Console.WriteLine("bye " + name);
+        }
+        public static void addition()
+        {
+            Console.WriteLine("What\'s your first number? ");
+            string firstnum = Console.ReadLine();
+            Console.WriteLine("What\'s your second number? ");
+            string secondnum = Console.ReadLine();
+            int first = Convert.ToInt32(firstnum);
+            int second = Convert.ToInt32(secondnum);
+            Console.WriteLine(first + second);
+
+        }
+        public static void catDog()
+        {
+            Console.WriteLine("Which do you prefer? Cats, or Dogs? ");
+            string answer = Console.ReadLine();
+            if (answer == "cats")
+            { Console.WriteLine("Meow!"); }
+            else if (answer == "dogs")
+            { Console.WriteLine("Bark Bark!"); }
+            else { Console.WriteLine("!!!WRONG!!!"); }
+        }
+        public static void oddEven()
+        {
+            Console.WriteLine("Hey! You! Give me a number!");
+            string number = Console.ReadLine();
+            int num = Convert.ToInt32(number);
+            int.Parse(number);
+            if (num % 2 == 0)
+            {
+                Console.Write("The Number Entered is an Even Number");
+                Console.Read();
+            }
+            else
+            {
+                Console.Write("The Entered Number is an Odd Number");
+                Console.Read();
+            }
+        }
+        public static void inches()
+        {
+            Console.WriteLine("I can convert Feet to Inches. Try me!");
+            Console.WriteLine("Type in a number of Feet");
+            string height = Console.ReadLine();
+
+            int feet = Convert.ToInt32(height);
+            Console.WriteLine(feet * 12);
+
+
+
+        }
+
+
+
+
+
+    }
+        
+}

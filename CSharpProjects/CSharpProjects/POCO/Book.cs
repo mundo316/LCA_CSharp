beaf1c44ef25805ea0bb882790d46d8733cd4b60
mundo316 <raymundo.garcia316@yahoo.com>
2019-07-17 20:59:57 -0500

    Restructuring Projects

---
+++ CSharpProjects/CSharpProjects/POCO/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace POCO
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+        public string SKU;
+        public string publisher;
+        public double price;
+
+        public Book(string Title, string Author, int Pages, string sku, string Publisher, double Price)
+        {
+            title = Title;
+            author = Author;
+            pages = Pages;
+            SKU = sku;
+            publisher = Publisher;
+            price = Price;
+
+        }
+
+
+    }
+}
---
+++ CSharpProjects/CSharpProjects/POCO/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace POCO
+{
+    class MainClass
+    {
+        DriversLicense firstone = new DriversLicense("ann", "smith", "f", 98765432);
+        Book firstbook = new Book("Sherlock Holmes", "Some Dude", 345, "SH34829472389", "PubCo", 9.99);
+
+                
+    }
+
+    class DriversLicense
+    {
+        public string firstname;
+        public string lastname;
+        public string Gender;
+        public int Licensenumber;
+        public string fullname;
+
+        //constructor
+        public DriversLicense(string fname, string lname, string gender, int licensenumber)
+        {
+            firstname = fname;
+            lastname = lname;
+            Gender = gender;
+            Licensenumber = licensenumber;
+        }
+
+        public string GetFullName()
+        {
+            fullname = firstname + lastname;
+            return fullname;
+
+        }
+    }
+
+    //book class is located in seperate file
+
+    class Airplane
+    {
+
+        public string Manufacturer;
+        public string Model;
+        public string Variant;
+        public int Capacity; //this should hold the seating capacity
+        public int Engines; //this should hold the number of engines
+
+        public Airplane()
+        {
+            Manufacturer = ".  ";
+            Model = ".  ";
+            Variant = ".  ";
+            Capacity = 0;
+            Engines = 0;
+        }
+    }
+}

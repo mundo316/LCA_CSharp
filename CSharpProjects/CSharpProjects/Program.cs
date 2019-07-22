a1d8791ef523d614d4baeb1ff16c255391c246b0
mundo316 <raymundo.garcia316@yahoo.com>
2019-07-21 23:11:06 -0500

    Checkpoint 1 added

--- CSharpProjects/CSharpProjects/Program.cs
+++ CSharpProjects/CSharpProjects/Program.cs
@@ -1,12 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TowersofHanoi
 {
     class MainClass
     {
+        static Dictionary<string, Stack<int>> Towers = new Dictionary<string, Stack<int>>();
+        //call upon constructed methods to run game
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Towers of Hanoi");
+            Console.WriteLine("Objective: Move the entire stack to row C without placing a bigger number onto a smaller number.");
+
+            StartingBoard();
+            
+            while (Towers["C"].Count != 4)
+            {
+                Printboard();
+                Gamemove();
+
+            }
+
+            Console.WriteLine("Congratulations! You Win!");
+           
+        }
+        public static void StartingBoard() //starting board
+        {
+            Towers.Add("A", new Stack<int>());
+            Towers.Add("B", new Stack<int>());
+            Towers.Add("C", new Stack<int>());
+
+            Towers["A"].Push(4);
+            Towers["A"].Push(3);
+            Towers["A"].Push(2);
+            Towers["A"].Push(1);
+        }
+
+        //game display
+        public static void Printboard()
+        {
+            foreach (var item in Towers.Keys)
+            {
+                int[] arrtower = Towers[item].ToArray();
+                Console.Write(item + "| ");
+                for(int i =arrtower.Length-1;i>=0;i--)
+                {
+                    Console.Write($"{arrtower[i]} ");
+                }
+                Console.WriteLine();
+            }   
+        }
+        public static bool Rules(string from, string to)
+        {
+            //checks for illegal moves, cant move on top of a smaller block
+            if (Towers[to].Count == 0  || Towers[to].Peek() > Towers[from].Peek())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //structure for processing each turn
+        public static void Gamemove()
+        {
+            Console.WriteLine("Select the stack to move from.");
+            string fromstack = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Select the stack to move to.");
+            string tostack = Console.ReadLine().ToUpper();
+
+            if (Rules(fromstack, tostack))
+            {
+                Towers[tostack].Push(Towers[fromstack].Peek());
+                Towers[fromstack].Pop();
+
+            }
+
         }
     }
+    
+
+
 }
---
+++ CSharpProjects/CSharpProjects/Readme.txt
@@ -0,0 +1,21 @@
+﻿This is a text version of the game Towers of Hanoi.
+
+OBJECTIVE:
+The objective is to move the stack to the end of the board without
+placing a larger unit over a smaller one, moving only one number at a 
+time.
+
+INSTRUCTIONS:
+To play this game the player must input which stack they are moving 
+the top unit from, and then press enter. The player will then be 
+prompted to input the stack they wish to move the unit to. Input
+the selection and then press enter. Continue making moves until all
+four units are in Row C. This is the end of the game and the Player
+wins!
+
+Ver 1.0.1 Bugs
+-The game will crash if the player inputs anything other than "A" "B" or "C"
+-Still looking for a proper place in the code to add an error message when 
+the player uses an illegal character. 
+-Haven't yet found a method of clearing the board after each turn so that
+the player does not get lost with a screen full of text.
